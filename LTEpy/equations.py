84cb528a7ecdec82a90b5fe3905195c50f683eee
Emiko <emikogardiner@berkeley.edu>
2023-07-10 17:05:04 -0500

    Finish Boltzmann_Gibbs.ninj.

--- LTEpy/equations.py
+++ LTEpy/equations.py
@@ -62,8 +62,23 @@ class Boltzmann_Gibbs(_LTE):
         self.Eii = atom.energy(ii)
         self.Ejj = atom.energy(jj)
 
-        # ---- Derived and internal parameters
-        self._pipj = None
+        # These values are calculated as needed by the class when the corresponding methods are called
+        self._pipj = None          #: Probability ratio of ith to jth energy levels
+        self._deltaE = None        #: Difference between ith and jth energy levels)
+        
+
+    @property
+    def _deltaE(self):
+        """ Calculate the difference between energy levels.
+        
+        """
+        if self._deltaE is None:
+            deltaE = self.Ejj - self.Eii
+            self._deltaE = deltaE
+
+        return deltaE
+
+
 
     @property
     def _pipj(self):
@@ -73,20 +88,27 @@ class Boltzmann_Gibbs(_LTE):
         p_i/p_j = exp[(E_j - E_i)/kT]
         """
         if self._pipj is None:
-            deltaE = self.Ejj - self.Eii
-            pipj = np.exp(deltaE/KBOLTZ/self.temp)
+            deltaE = self._deltaE
+            pipj = np.exp(self.deltaE/KBOLTZ/self.temp)
             self._pipj = pipj
 
         return self._pipj
 
-    def _ninj(self):
-        """ Calculate the ratio of number densities n_i/n_j between energy levels i and j.
+
+    def ninj(self):
+        """ Calculate the ratio of number densities n_i/n_j between energy levels i and j
+        from the probability ratio and degeneracies.
         
+        n_i/n_j = (g_1/g_2) * exp[(E_j - E_i)/kT] = (g_1/g_2) * (p_i/p_j)
         """
+
         atom = self.atom
+        gigj = self.gii / self.gjj
+        pipj = self._pipj
+        self.ninj = gigj * pipj
 
-
-
+        return self.ninj
+        
 
         
 
@@ -123,6 +145,7 @@ class Atom():
 
     def add_level(self, degen, energy, level):
         pass 
+
 
 
 
