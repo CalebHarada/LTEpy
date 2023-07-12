66a078cd857ce34bd223d675c31ea5595b5d5495
Caleb Harada <calebharada@Calebs-MacBook-Pro-16in.local>
2023-07-12 17:58:29 -0500

    minor bug fix

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Gardiner, Emiko C., Harada, Caleb K.'
 root_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
