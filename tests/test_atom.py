555a2106657d5e4cfa6f408f89bc2c24630f784a
Emiko <emikogardiner@berkeley.edu>
2023-07-13 11:02:54 -0500

    Start atom and boltzmann factor tests.

---
+++ tests/test_atom.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pytest
+
+import sys
+sys.path.append('../')
+
+from LTEpy import lte, atom
+
+def test_basic_atom():
+    """ 
+    
+    """
---
+++ tests/test_boltzmann_factor.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pytest
+
+import sys
+sys.path.append('../')
+
+from LTEpy import lte, atom
+from LTEpy.constants import EVOLT
+
+def test_
