d612f6f3864b148ae0857d23b42678a4d667a90d
Caleb Harada <calebharada@Calebs-MacBook-Pro-16in.local>
2023-07-13 18:11:47 -0500

    added more planck tests

--- tests/test_planck.py
+++ tests/test_planck.py
@@ -2,7 +2,7 @@ from LTEpy import lte
 import pytest
 
 def test_set_temp():
-    """Test setting the temperature.
+    """Test changing the temperature.
 
     """
 
@@ -11,6 +11,7 @@ def test_set_temp():
     # try changing to a positive temp
     new_temp = 5000
     planck.set_temp(new_temp)
+
     assert planck.temp == pytest.approx(new_temp, abs=1e-3)
 
     # try changing to a negative temp
@@ -25,13 +26,34 @@ def test_set_temp():
 
 
 def test_compute_B_nu():
-    """Test computation of B_nu.
+    """Test computing B_nu.
 
     """
 
     planck = lte.Planck(1000)  # new instance of Planck
 
+    nu = 6e14  # frequency in Hz
+    B_nu_expected = 9.94e-16  # from Wolfram Alpha
+    B_nu = planck.compute_B_nu(nu)
+
+    assert B_nu == pytest.approx(B_nu_expected, abs=0.1*B_nu_expected)
+
+
+def test_compute_B_lambda():
+    """Test computing B_lambda.
+
+    """
+
+    planck = lte.Planck(1000)  # new instance of Planck
+
+    wl = 5e-5  # wavelength in cm
+    B_lambda_expected = 0.001213  # from Wolfram Alpha
+    B_lambda = planck.compute_B_lambda(wl)
+
+    assert B_lambda == pytest.approx(B_lambda_expected, abs=0.1*B_lambda_expected)
 
 
 if __name__ == '__main__':
     test_set_temp()
+    test_compute_B_nu()
+    test_compute_B_lambda()
