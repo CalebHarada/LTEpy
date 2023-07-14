1d734ae92faa336948e768b28797f3a160f2ebe8
Emiko <emikogardiner@berkeley.edu>
2023-07-13 23:00:30 -0500

    Write test for basic 1000K boltzmann factor calculation.

--- tests/test_boltzmann_factor.py
+++ tests/test_boltzmann_factor.py
@@ -2,9 +2,36 @@ import numpy as np
 import pytest
 
 import sys
-sys.path.append('../')
+sys.path.append('../LTEpy')
 
 from LTEpy import lte, atom
 from LTEpy.constants import EVOLT
 
-def test_
+
+
+
+def test_bfact():
+    """ Check that the boltzman factors are calculated correctly for the
+    hydrogen atom at 1000 K.
+    
+    """
+
+    # hydrogen boltzmann factors at 1000 K
+    hbf_1000K = np.array([5.532e18, 3.136e7, 4.006e2, 1.562e0, 5.343e-1])
+    # hydrogen atom
+    hydrogen=atom.Hydrogen(levels=np.arange(1,6))
+
+    temp = 1000 # Kelvin
+    hbf = lte.Boltzmann_Factor(temp, hydrogen)
+    assert np.all(hbf_1000K == pytest.approx(hbf.bfact))
+
+def test_zero_Kelvin():
+    """ Make sure the code raises a ValueError if you try to use 0 Kelvin temperature.
+    
+    """
+    pass
+
+    
+if __name__ == "__main__":
+    test_zero_Kelvin()
+    test_bfact()
