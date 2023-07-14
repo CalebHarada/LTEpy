0da8473d391a57b12d5ed9334d6f6af5c7f79833
Emiko <emikogardiner@berkeley.edu>
2023-07-13 23:38:39 -0500

    Write test to print any bad energy levels for hydrogen at 1000 K.

--- tests/test_boltzmann_factor.py
+++ tests/test_boltzmann_factor.py
@@ -9,7 +9,6 @@ from LTEpy.constants import EVOLT
 
 
 
-
 def test_bfact():
     """ Check that the boltzman factors are calculated correctly for the
     hydrogen atom at 1000 K.
@@ -17,13 +16,17 @@ def test_bfact():
     """
 
     # hydrogen boltzmann factors at 1000 K
-    hbf_1000K = np.array([5.532e18, 3.136e7, 4.006e2, 1.562e0, 5.343e-1])
+    hbf_1000K = np.array([3.475e68, 1.365e17, 4.127e7, 1.922e4, 551.6])
     # hydrogen atom
     hydrogen=atom.Hydrogen(levels=np.arange(1,6))
 
     temp = 1000 # Kelvin
-    hbf = lte.Boltzmann_Factor(temp, hydrogen)
-    assert np.all(hbf_1000K == pytest.approx(hbf.bfact))
+    bfact = lte.Boltzmann_Factor(temp, hydrogen).bfact
+    good = np.isclose(bfact, hbf_1000K, rtol=1e-3)
+    bads = np.logical_not(good)
+    if np.any(bads):
+        err = f"Levels {hydrogen.levels[bads]=} with {bfact[bads]=} should have boltzmann factors {hbf_1000K[bads]}"
+        raise ValueError(err)
 
 def test_zero_Kelvin():
     """ Make sure the code raises a ValueError if you try to use 0 Kelvin temperature.
